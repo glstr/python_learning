418f5300219282c868fd2c8b224712e8fb842951
Peng <pengbaojiang>
2018-03-06 10:09:26 +0800

    add send_msg

---
+++ sock_tool.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+"""
+@brief send data with socket
+"""
+import socket
+
+def send_msg(host, port, data):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(host, port)
+	s.sendall("hello world")
+	data = s.recv(1024)
+	s.close()
+	print "received", repr(data)
+
+
