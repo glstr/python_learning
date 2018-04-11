77a80bb9f8ff3ec773f3fcbe5a8965e9d4ecac6c
glstr <pengbaojiang>
2018-04-12 00:22:54 +0800

    sync

--- requests_func.py
+++ requests_func.py
@@ -3,10 +3,3 @@
 
 import requests
 
-local_host = "http://cp01-pengbaojiang.epc.baidu.com:8882/api/1.0/rooms.info"
-def test_liveshow_info(room_id):
-	parameter = {"room_id": room_id}
-	r = requests.post(local_host, parameter)
-	data =  r.json()
-	print data["info"]
-test_liveshow_info(554110)
