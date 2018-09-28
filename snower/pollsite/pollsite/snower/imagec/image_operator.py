9e380dbbb8de54fc6e7315fcbf7867f3f02d5101
glstr <glstrsc@163.com>
2018-09-28 16:48:14 +0800

    add image operator

--- snower/pollsite/pollsite/snower/imagec/image_operator.py
+++ snower/pollsite/pollsite/snower/imagec/image_operator.py
@@ -4,35 +4,42 @@
     @brief: image operator
     @author: glstr
 '''
+import sys
 
 from PIL import Image
 
 
-def divide(path):
-    im = Image.open(path) 
-    width = im.width
-    height = im.height
-
-    print width
-    print height
-
-    box = (0, 0, width/2, height/2)
-    box_1 = (0, height/2, width/2, height)
-    box_2 = (width/2, 0, width, height/2)
-    box_3 = (width/2, height/2, width, height)
-
-    _divide(box, im, "0")
-    _divide(box_1, im, "1")
-    _divide(box_2, im, "2")
-    _divide(box_3, im, "3")
-
-
-def _divide(box, image, filename):
-    region = image.crop(box)
-    region.save(filename+'.jpg')
+class ImageOpeor:
+    def __init__(self):
+        return 
+    
+    def divide(self, path):
+        im = Image.open(path) 
+        width = im.width
+        height = im.height
+    
+        print width
+        print height
+    
+        box = (0, 0, width/2, height/2)
+        box_1 = (0, height/2, width/2, height)
+        box_2 = (width/2, 0, width, height/2)
+        box_3 = (width/2, height/2, width, height)
+    
+        self._divide(box, im, "0")
+        self._divide(box_1, im, "1")
+        self._divide(box_2, im, "2")
+        self._divide(box_3, im, "3")
+    
+    def _divide(self, box, image, filename):
+        region = image.crop(box)
+        region.save(filename+'.jpg')
 
 
 if __name__ == '__main__':
-    path = '/Users/pengbaojiang/pengbaojiang/code/python_src/python_learning/image_client/img.jpg'
-    divide(path)
+    if len(sys.argv) != 2:
+        print "param error"
+    img_ope = ImageOpeor()       
+    path = sys.argv[1] 
+    img_ope.divide(path)
 
