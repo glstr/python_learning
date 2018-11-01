0442f1c28edb4e5054a7a649c8d278fb15f340a6
glstr <glstrsc@163.com>
2018-11-01 23:59:21 +0800

    remove invalid code

--- snower/pollsite/pollsite/snower/imagec/image_case.py
+++ snower/pollsite/pollsite/snower/imagec/image_case.py
@@ -4,6 +4,7 @@
 '''
     @brief: Service for image operation and tool of make experiment.
 '''
+import os
 from pprint import pprint
 import sys
 
@@ -11,77 +12,11 @@ from pyecharts import Line
 
 import common
 import imager 
-import image_manager as img_mar
 import image_operator as img_ope
-import img_redisc as redisc
 import mutual_infor as mi
 
 
 funcs = {}
-
-
-class ImageClient():
-    def __init__(self):
-        return 
-
-    def load_config(self):
-        name = 'snow'
-        self.img_mana = img_mar.ImageManager(name)
-        self.img_mana.load()
-        self.mi_base = mi.MIBase() 
-        return 
-
-    def case_one(self):
-        # get img path        
-        files_path = self._get_files_path()
-        # caculate entropy
-        key = 'pcloud:entropy'
-        entropy_dir = {}
-        for file_path in files_path:
-            gray_data = self._get_graydata(file_path)
-            entropy = self.mi_base.compute_entropy(gray_data)                               
-            file_name = common.get_filename(file_path)
-            entropy_dir[file_name] = entropy
-        redisc.hmset(key, entropy_dir)
-        return  
-
-    def case_two(self):
-        # get img path
-        files_path = self._get_files_path()  
-        # files_num = len(files_path)
-        # random_num = common.random_num(0, files_num-1)
-        # caculate entropy
-        file_path = files_path.pop()
-        data_a = self._get_graydata(file_path)
-        file_name = common.get_filename(file_path)
-        key = 'pcloud:mi:' + file_name
-        mi_dir = {}
-        for file_path in files_path:
-            file_name = common.get_filename(file_path)
-            data_b = self._get_graydata(file_path)
-            img_mi = self.mi_base.compute_mi(data_a, data_b)
-            mi_dir[file_name] = img_mi
-        redisc.hmset(key, mi_dir)
-        return 
-
-    def case_three(self):
-        files_path = self._get_files_path()
-        for file_path in files_path:
-            gray_data = self._get_graydata(file_path)
-            for new_file_path in files_path:
-                gray_data = _get_graydata(new_file_path)
-                img_mi = self.mi_base.compute_mi(data_a, data_b) 
-        return 
-
-    def _get_graydata(self, file_path):
-        img = imager.Imager(file_path)
-        return img.get_graydata()
-
-    def _get_files_path(self):
-        return self.img_mana.get_allimages_path()
-
-    def start(self):
-        return 
 
 
 def img_info(param):
@@ -157,7 +92,7 @@ def mutual_info_ex(param):
 
 
 def mutual_info_ex_f(param):
-    dir_path = "range/" 
+    dir_path = os.path.dirname(param)
     all_path = ""
     with open(param) as f:
         for line in f.readlines():
@@ -234,6 +169,7 @@ def usage():
     for i in funcs.keys():
         print i
     print "example:"
+    print "python image_case.py m_info_f 'range/index.txt'"
 
 
 if __name__ == '__main__':
@@ -245,5 +181,7 @@ if __name__ == '__main__':
         param = sys.argv[2]
         if funcs.has_key(method):
             funcs[method](param)
+        else:
+            usage()
 
 
