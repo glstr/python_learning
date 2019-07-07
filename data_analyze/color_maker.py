2d2393dc202dce7cf34b28f14e2f30a2980e1fae
glstr <glstrsc@163.com>
2019-07-07 07:51:53 -0700

    add color_maker

---
+++ data_analyze/color_maker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys 
+
+def color_make(fine_in, file_out):
+    '''
+    brief color_make user one feature of file to 
+    generate color(RGB);
+    file_in: x y z f;
+    file_out: x y z r g b f;
+    '''
+    
+def read_file(file_in):
+    return 
+
+def write_file(file_out):
+    return 
+
+def _color_make():
+    return 
+
+if __name__ == '__main__':
+    print "hello world"
